3eb35193218f95d13d2ff1d6b51089a383d5f9f6
Jordan Onwodi <Jonwodi@hotmail.co.uk>
2022-07-07 11:23:21 +0100

    form complete

---
+++ components/Form.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+export default function Form() {
+  // attribute settings and text for input, label and paragraph tags
+  const formSettings = [
+    {
+      nameId: "name",
+      type: "text",
+      placeholder: "Enter your name",
+      text: "Name *",
+    },
+    {
+      nameId: "email",
+      type: "email",
+      placeholder: "Enter your email address",
+      text: "Email *",
+    },
+  ];
+
+  return (
+    <form
+      id="registration"
+      action=""
+      method="post"
+      className="flex flex-col flex-wrap sm:items-start smUp:items-center gap-y-10 max-w-full sm:text-left smUp:text-center">
+      {formSettings.map((settings) => (
+        <label for={settings.nameId} form="registration">
+          <p className="text-[#1A75BC]">{settings.text}</p>
+          <input
+            id={settings.nameId}
+            type={settings.type}
+            name={settings.nameId}
+            required="required"
+            form="registration"
+            placeholder={settings.placeholder}
+            className="sm:placeholder:text-left smUp:placeholder:text-center text-black border-[3px] placeholder:text-slate-500 border-black focus:border-[#1A75BC]"
+          />
+        </label>
+      ))}
+      <label for="image" form="registration">
+        <p className="text-[#1A75BC]">Upload your image *</p>
+        <input
+          id="image"
+          type="file"
+          name="image"
+          required="required"
+          form="registration"
+          className="sm:placeholder:text-left smUp:placeholder:text-center"
+        />
+      </label>
+
+      <button
+        form="registration"
+        formMethod="post"
+        type="submit"
+        name="send"
+        className="bg-[#1A75BC] hover:bg-[#1A75BC]/80 text-white sm:px-5 smUp:px-20">
+        Send
+      </button>
+    </form>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      smUp: { max: "640px" },
+      mdUp: { max: "768px" },
+      lgUp: { max: "1024px" },
+      xlUp: { max: "1280px" },
+    },
     extend: {},
   },
   plugins: [],
